5f50e9ef45c6df74e50cf18bbfe36158495b03f7
CAFE INTERNET <ubuwill@gmail.com>
2020-07-18 10:57:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,208 @@
+var verbos = ["aceptar", "agregar", "admirar", "admitir", "aconsejar", "pagar", "aceptar", "alertar", "permitir", "divertir", "analizar", "anunciar ", " molestar ", " responder ", " disculparse ", " aparecer ", " aplaudir ", " apreciar ", " aprobar ", " discutir ", " organizar ", " arrestar ", " llegar ", " preguntar ", "adjuntar", "atacar", "intentar", "asistir", "atraer", "evitar", "volver", "hornear", "equilibrar", "prohibir", "golpear", "desnudar", "murciélago", " bañarse ", " batalla ", " haz ", " suplicar ", " comportarse ", " pertenecer ", "lejía", "bendecir", "ciego", "parpadeo", "mancha", "sonrojo", "alardear", "hervir", "perno", "bomba", "libro", "aburrir", "pedir prestado", " rebote ", " arco ", " caja ", " freno ", " rama ", " respirar ", " hematoma ", " cepillo ", " burbuja ", " golpe ", " quemar ", " enterrar ", "zumbido", "calcular", "llamar", "acampar", "cuidar", "llevar", "tallar", "causar", "desafiar", "cambiar", "cargar", "perseguir", "engañar", " check ", " cheer ", " chew ", " choke ", " chop ", " Claim ", " clap ", "clean ", " clear ", " clip ", " close ", " coach ", " coil ", " collect ", " color ", " comb ", " command ", " comunicar ", " comparar ", " competir ", "quejarse", "completar", "concentrarse", "preocuparse", "confesar", "confundir", "conectar", "considerar", "constar", "contener", "continuar", "copiar", " correcto ", " tos ", " cuenta ", " tapa ", " grieta ", " choque ", " gatear ", " cruzar ", " aplastar ", " llorar ", " curar ", " curl ", " curva ", "ciclo", "presa", "daño", "danza", "reto", "decaer ", " engañar ", " decidir ", " decorar ", " retrasar ", " deleitar ", " entregar ", " depender ", " describir ", " desierto ", " merecer ", " destruir ", " detectar ", "desarrollar", "no estar de acuerdo", "desaparecer", "desaprobar", "desarmar", "descubrir", "no me gusta", "dividir", "doble", "dudar", "arrastrar", "drenar", " soñar ", " vestir ", " gotear ", " soltar ", " ahogarse ", " tambor ", " seco ", " polvo ", " ganar ", " educar ", " avergonzar ", " emplear ", " vaciar ", "alentar", "finalizar", "disfrutar", "entrar", "entretener", "escapar", "examinar", "excitar", "excusar", "hacer ejercicio", "existir", "expandir", "esperar", "explicar", "explotar", "extender", "extenderse", "desvanecerse", " fail ", " fancy ", " fasten ", " fax ", " fear ", " fence ", " fetch ", " file ", " fill ", " film ", " fire ", " fit ", "arreglar", "aletear", "destellar", "flotar", "inundar", "fluir", "flor", "doblar", "seguir", "tonto", "forzar", "formar", "encontrado ", " marco ", " asustar ", " freír ", " reunir ", " mirar ", " brillar ", " pegar ", "agarrar ", " rallar ", " engrasar ", " saludar ", " sonreír ", " agarrar ", " gemir ", " garantizar ", " proteger ", " adivinar ", " guiar ", " martillo ", " mano ", "manejar", "colgar", "pasar", "acosar", "dañar", "odiar", "perseguir", "cabeza", "curar", "apilar", "calentar", "ayudar", " enganchar ", " saltar ", " esperar ", " flotar ", " abrazar ", " tararear ", " cazar ", " apurarse ", " identificar ", " ignorar ", " imaginar ", " impresionar ", " mejorar ", "incluir", "aumentar", "influir", "informar", "inyectar", "dañar", "instruir", "intención", "interés", "interferencia", "interrupción", "presentación", "invención", "invitación", "irritación", "picazón", "cárcel", "atasco", "trote", " unirse ", " broma ", " juez ", " hacer malabares ", " saltar ", " patear ", " matar ", " besar ", " arrodillarse ", " tejer ", " golpear ", " nudo ", " etiqueta ", "land", "last", "laugh", "launch", "learn", "level", "license", "lick", "lie", "lighten", "like", "list", " escuchar ", " en vivo ", " cargar ", " bloquear ", " largo ", " mirar ", " amar ", " hombre ", "administrar", "marchar", "marcar", "casarse", "emparejar", "aparearse", "importar", "medir", "entrometerse", "derretir", "memorizar", "reparar", "desordenar", " arriba ", " leche ", " mina ", " señorita ", " mezclar ", " gemir ", " amarrar ", " llorar ", " moverse ", " enredarse ", " taza ", " multiplicarse ", " asesinato ", "clavo", "nombre", "necesidad", "nido", "asentimiento", "nota", "aviso", "número", "obedecer", "objeto", "observar", "obtener", " ocurrir ", " ofender ", " oferta ", " abrir ", " orden ", " desbordamiento ", " deber ", " propio ", "paquete ", " paleta ", " pintura ", " parquear ", " parte ", " pasar ", " pegar ", " palpar ", " pausar ", " picotear ", " pedalear ", " pelar ", " peep ", "realizar", "permitir", "teléfono", "recoger", "pellizcar", "pino", "lugar", "plan", "planta", "jugar", "por favor", "enchufar", " punto ", " empujar ", " pulir ", " pop ", " poseer ", " publicar ", " verter ", " practicar ", " orar ", " predicar ", " preceder ", " preferir ", " preparar ", "presente", "preservar", "presionar", "pretender", "prevenir", "pinchar", "imprimir", "produce", "programa", "promete", "protege", "proporciona", "tira", "bombea", "perfora", "perfora", "castiga", "empuja", "pregunta", "hace cola", " raza ", " irradiar ", " lluvia ", " subir ", " alcanzar ", " darse cuenta ", " recibir ", " reconocer ", " registrar ", " reducir ", " reflejar ", " rechazar ", "arrepentirse", "reinar", "rechazar", "regocijarse", "relajarse", "liberar", "confiar", "permanecer", "recordar", "recordar", "eliminar", "reparar", "repetir", " reemplazar ", " responder ", " informar ", " reproducir ", "solicitud ", " rescate ", " retiro ", " devolución ", " rima ", " enjuague ", " riesgo ", " robo ", " roca ", " rodadura ", " podredumbre ", " frotación ", " ruina ", "rule", "rush", "sack", "sail", "satisfize", "save", "saw", "scare", "scatter", "scold", "scorch", "scrape", " rascar ", " gritar ", " atornillar ", " garabatear ", " fregar ", " sellar ", " buscar ", " separar ", " servir ", " asentar ", " sombrear ", " compartir ", " afeitar ", "refugio", "escalofrío", "shock", "tienda", "encogimiento de hombros", "suspiro", "signo", "señal ", " sin ", " sip ", " ski ", " skip ", " slap ", " slip ", " slow ", " smash ", " olor ", " sonrisa ", " humo ", " arranque ", "estornudar", "oler", "roncar", "nieve", "remojar", "calmar", "sonido", "sobrante", "chispa", "chispa", "hechizo", "derrame", " estropear ", " manchar ", " rociar ", " brotar ", " aplastar ", " chirriar ", " chirriar ", " apretar ", " manchar ", " estampar ", " mirar ", " comenzar ", " quedarse ", "steer", "step", "agitar", "stitch", "stop", "store", "strap", "fortalecer ", " estirar ", " pelar ", " trazo ", " cosas ", " restar ", " tener éxito ", " chupar ", " sufrir ", " sugerir ", " traje ", " suministro ", " soporte ", "supongamos", "sorpresa", "envolvente", "sospechoso", "suspender", "cambiar", "hablar", "domesticar", "tocar", "probar", "burlarse", "teléfono", " tentar ", " aterrorizar ", " probar ", " agradecer ", " descongelar ", " marcar ", " cosquillear ", " atar ", " tiempo ", " propina ", " neumático ", " tocar ", " recorrer ", "remolque", "rastreo", "comercio", "tren", "transporte", "trampa", "viaje ", " tratar ", " temblar ", " truco ", " viaje ", " trote ", " problema ", " confianza ", " prueba ", " tirón ", " caída ", " giro ", " giro ", "tipo", "desvestirse", "desabrochar", "unir", "desbloquear", "desempaquetar", "desordenar", "usar", "desaparecer", "visitar", "gritar", "esperar", " caminar ", " deambular ", " querer ", " calentar ", " advertir ", " lavar ", " desechar ", " mirar ", " agua ", " agitar ", " pesar ", " bienvenido ", " quejarse ", "whip", "whirl", "whisper", "whistle", "wink", "wipe", "wish", "bamboleo ", " maravilla ", " trabajo ", " preocupación ", " envoltura ", " choque ", " lucha ", " ondulación ", " rayos X ", " bostezo ", " grito ", " zip ", " enfocar"];
+var lo = "Art of hosting Ecosistemas Poder Colaboración Organismos vivos Multiples niveles Adaptable Exploraciones energía emergente Búsquedas naturales Autoorganización Historias Hackear Cocinar Alianzas Errores Información Investigaciones Asteroides Dinosaurios Estudios Toma de decisiones Gobierno Instituciones Públicas Colectivos Organizaciones sociales Apps Negocios Pacientes Clientes Usuarios Usuarias Usuarxs Usuares Mascaras Boicot / Botcoin Policia Resistencias Lluvias Aviones Volcanes Dinero Gigantesca Estructuras Robot Creadorxs Programación Secretos Silencio político Acuerdos Planes Prostitución Lanzan Plataforma Documentos Última Lugares Resistieron Se enfrentan Averno Tropical Rápido Técnicas Prender Todo Oportunidad Repollo Árboles Faces Pensamientos Curación Desarrollo Magía Giro Social Estar de acuerdo Incribles Amor Miedo Hambre Sueños Caminar Dormir Pensar Se lograron La lucha continua Nativo Missing lives Artesanal Música Carteles Fungi Kingdom Celebración Lenguas Nomadas Poder Orden Control Creer Mentir Ganar Perder Luchar Vigilar Contaminar Reciclar Innovar Adoctrinar Programar Normalizar Estandarizar Rotular Enumerar Cifrar Obedecer Seguir Oponerse lógica paralelo infinito red mente colectiva ser consciencia oración "
+
+
+$(function () {
+
+var palabra = ['Art', 'of', 'hosting', 'Ecosistemas',
+  'Poder', 'Colaboración', 'Organismos', 'vivos',
+  'Multiples', 'niveles', 'Adaptable', 'Exploraciones',
+  'energía', 'emergente', 'Búsquedas', 'naturales',
+  'Autoorganización', 'Historias', 'Hackear', 'Cocinar',
+  'Alianzas', 'Errores', 'Información', 'Investigaciones',
+  'Asteroides', 'Dinosaurios', 'Estudios', 'Toma',
+  'de', 'decisiones', 'Gobierno', 'Instituciones',
+  'Públicas', 'Colectivos', 'Organizaciones', 'sociales',
+  'Apps', 'Negocios', 'Pacientes', 'Clientes',
+  'Usuarios', 'Usuarias', 'Usuarxs', 'Usuares',
+  'Mascaras', 'Boicot', '/', 'Botcoin',
+  'Policia', 'Resistencias', 'Lluvias', 'Aviones',
+  'Volcanes', 'Dinero', 'Gigantesca', 'Estructuras',
+  'Robot', 'Creadorxs', 'Programación', 'Secretos',
+  'Silencio', 'político', 'Acuerdos', 'Planes',
+  'Prostitución', 'Lanzan', 'Plataforma', 'Documentos',
+  'Última', 'Lugares', 'Resistieron', 'Se',
+  'enfrentan', 'Averno', 'Tropical', 'Rápido',
+  'Técnicas', 'Prender', 'Todo', 'Oportunidad',
+  'Repollo', 'Árboles', 'Faces', 'Pensamientos',
+  'Curación', 'Desarrollo', 'Magía', 'Giro',
+  'Social', 'Estar', 'de', 'acuerdo',
+  'Incribles', 'Amor', 'Miedo', 'Hambre',
+  'Sueños', 'Caminar', 'Dormir', 'Pensar',
+  '', 'Se', 'lograron',
+  'La', 'lucha', 'continua',
+  'Nativo', 'Missing', 'lives',
+  'Artesanal', 'Música', 'Carteles',
+  'Fungi', 'Kingdom', 'Celebración',
+  'Lenguas', 'Nomadas', 'Poder',
+  'Orden', 'Control', 'Creer',
+  'Mentir', 'Ganar', 'Perder',
+  'Luchar', 'Vigilar', 'Contaminar',
+  'Reciclar', 'Innovar', 'Adoctrinar',
+  'Programar', 'Normalizar', 'Estandarizar',
+  'Rotular', 'Enumerar', 'Cifrar',
+  'Obedecer', 'Seguir', 'Oponerse',
+  '', 'lógica', 'paralelo',
+  'infinito', 'red', 'mente',
+  'colectiva', 'ser', 'consciencia',
+  'oración', 'Correr', 'lentamente',
+  'Volar', 'acuaticamente',
+  'Pensar', 'vacuamente',
+  
+  'Mirar a ciegas',
+  'Programar dioses'
+  ]
+
+
+  var quotes = [{
+    quote: 'Java is to JavaScript what Car is to Carpet.',
+    author: 'Chris Heilmann',
+    link: '#'
+  }, {
+    quote: 'It\'s hard enough to find an error in your code when you\'re looking for it, it\'s even harder when you\'ve assumed your code is error-free.',
+    author: 'Steve McConnell',
+    link: 'https://en.wikipedia.org/wiki/Steve_McConnell'
+  }, {
+    quote: 'If debugging is the process of removing software bugs, then programming must be the process of putting them in.',
+    author: 'Edsger Dijkstra',
+    link: 'https://en.wikipedia.org/wiki/Edsger_W._Dijkstra'
+  }, {
+    quote: 'Walking on water and developing software from a specification are easy if both are frozen.',
+    author: 'Edward V Berard',
+    link: 'https://en.wikiquote.org/wiki/Edward_V._Berard'
+  }, {
+    quote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    author: 'Brian Kernighan',
+    link: 'https://en.wikipedia.org/wiki/Brian_Kernighan'
+  }, {
+    quote: 'It\'s not at all important to get it right the first time. It\'s vitally important to get it right the last time.',
+    author: 'David Thomas',
+    link: 'https://en.wikipedia.org/wiki/Dave_Thomas_(programmer)'
+  }, {
+    quote: 'Should array indices start at 0 or 1? My compromise of 0.5 was rejected without, I thought, proper consideration.',
+    author: 'Stan Kelly-Bootle',
+    link: 'https://en.wikipedia.org/wiki/Stan_Kelly-Bootle'
+  }, {
+    quote: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.',
+    author: 'Rick Osborne',
+    link: '#'
+  }, {
+    quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    author: 'Martin Fowler',
+    link: 'https://en.wikipedia.org/wiki/Martin_Fowler'
+  }, {
+    quote: 'Software sucks because users demand it to.',
+    author: 'Nathan Myhrvold',
+    link: 'https://en.wikipedia.org/wiki/Nathan_Myhrvold'
+  }, {
+    quote: 'Linux is only free if your time has no value. ',
+    author: 'Jamie Zawinski',
+    link: 'https://en.wikipedia.org/wiki/Jamie_Zawinski'
+  }, {
+    quote: 'The first 90p of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.',
+    author: 'Tom Cargill',
+    link: 'https://en.wikipedia.org/wiki/Ninety-ninety_rule'
+  }, {
+    quote: 'Programming can be fun, so can cryptography, however they should not be combined.',
+    author: 'Ben Shneiderman',
+    link: 'https://en.wikipedia.org/wiki/Ben_Shneiderman'
+  }, {
+    quote: 'Copy and paste is a design error.',
+    author: 'David Parnas',
+    link: 'https://en.wikipedia.org/wiki/David_Parnas'
+  }, {
+    quote: 'Before software can be reusable it first has to be usable.',
+    author: 'Ralph Johnson',
+    link: 'https://en.wikipedia.org/wiki/Ralph_Johnson_(computer_scientist)'
+  }, {
+    quote: 'When someone says \'I want a programming language in which I need only say what I want done\' give him a lollipop.',
+    author: 'Alan Perlis',
+    link: 'https://en.wikipedia.org/wiki/Alan_Perlis'
+  }, {
+    quote: 'Computers are good at following instructions, but not at reading your mind.',
+    author: 'Donald Knuth',
+    link: 'https://en.wikipedia.org/wiki/Donald_Knuth'
+  }, {
+    quote: 'Any code of your own that you haven\'t looked at for six or more months might as well have been written by someone else.',
+    author: 'Eagleson\'s law',
+    link: '#'
+  }];
+
+  // vars
+  var i = 0;
+  var x = 0;
+  var result = [];
+  var $element = $('#quote');
+  var $tweet = $('#tweet-wrapper');
+  var htmlOutput;
+  // IIFE - get array of random numbers
+  (function () {
+
+    var minNum = 0;
+    var maxNum = quotes.length;
+    var randomNum = 0;
+
+    while (result.length < maxNum) {
+      randomNum = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+      // if random number doesnt exists in array push it to array
+      if (result.indexOf(randomNum) > -1) continue;
+      result.push(randomNum);
+    }
+  })();
+
+
+  function output() {
+    // x iterates quotes obj
+    if (x < quotes.length) {
+      var num = result[x];
+      htmlOutput = '<p>' + "hola" + '</p>';
+
+      if (quotes[num].link == '#') {
+        htmlOutput = '<p>' + "" + '</p>';
+
+      } else {
+        htmlOutput = '<p>' + palabra[num] + '</p>' 
+        
+        // '<footer class="brackets author">' + quotes[num].author + '<span class="cursor blink">&#9646;</span></footer>';
+
+      }
+    } else {
+      htmlOutput = '<div class="warning"><span>WARNING</span><p> NO MAS ACONTECIMIENTOS PARA HOY <span class="cursor blink">&#9646;</span></p></div>';
+    }
+    //$element.html(htmlOutput);
+    x++;
+    return render();
+  };
+
+  output();
+
+
+  var isTag, char, text;
+
+  function render() {
+    //console.log('i',i);
+
+    text = htmlOutput.slice(0, i++);
+
+    if (text === htmlOutput) return i = 0;
+
+    $element.html(text + '&#9646;');
+
+    char = text.slice(-1);
+
+    if (char === '<') isTag = true;
+    if (char === '>') isTag = false;
+
+    if (isTag) return render();
+
+    return setTimeout(render, 20);
+  };
+
+  $('#newQuoteBtn').on('click', function () {
+
+    output();
+
+  });
+
+
+});
